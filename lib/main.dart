7ce34bf8acce37d76061831f83d9d1a386f6c3db
BIPLOB-SHIL <biplobshil.duet@gmail.com>
2023-07-03 21:44:37 +0600

    The file is created for live test 08

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Biplob Shil",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Biplob Shil"),
+        backgroundColor: Colors.amber,
+        elevation: 5,
+      ),
+      body: Center(child: const Text("Body")),
+
+    );
+  }
+}
+
+
